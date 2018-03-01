7bad5d45df56d9e7dcd1146e57201b0d38842d09
A Gadon <apgadon@gmail.com>
2018-03-01 13:31:13 -0800

    [0.1.6] Set up Priority Queue structure

--- AirportSimulator/src/PriorityQueue.java
+++ AirportSimulator/src/PriorityQueue.java
@@ -1,4 +1,23 @@
-
-public class PriorityQueue<T> {
-
+//This class header ensures that elements of PriorityQueue can be compared AND updated
+public class LinkedPriorityQueue<T extends Comparable<T> & Updatable>{
+  
+  public void enqueue(T new){
+    //@TODO AS HEAP
+  }
+  
+  public T dequeue(){
+    //@TODO AS HEAP
+  }
+  
+  public boolean isEmpty(){
+    //@TODO AS HEAP
+  }
+  
+  public void clear(){
+    //@TODO AS HEAP
+  }
+  
+  public void updateValues(){
+    //@TODO Use update() on each Node.val
+  }
 }
