f013a5b90cfa50d669c2bab86658854fca0f3f9d
apgadon <apgadon@gmail.com>
2018-03-10 15:59:20 -0800

    [0.2.1] Temporary changes to driver. Queue fixed

--- AirportSimulator/src/Driver.java
+++ AirportSimulator/src/Driver.java
@@ -1,6 +1,8 @@
 
 public class Driver {
-public static void main(String[] args) {
-
+	public static void main(String[] args) {
+		Queue<String> x = new Queue<>();
+		x.enqueue("balls");
+		System.out.println(x.dequeue() + "\n" + x.dequeue() + "\n" + x.dequeue());
 	}
 }
--- AirportSimulator/src/Queue.java
+++ AirportSimulator/src/Queue.java
@@ -2,43 +2,46 @@
 //implementation of a queue using a singly linked list
 public class Queue<T> {
 
-  Node<T> head;  //head points at the first Node
-  Node<T> tail;  //tail points at the last Node
+  Node<T> head;  //head is EQUAL TO the first Node
+  Node<T> tail;  //tail is EQUAL TO the last Node
   
   //initializes queue as empty
   public Queue(){
-    head.next = null;
-    tail.next = null;
+	  head = null;
+	  tail = null;
   }
   
   //adds a Node to the end of the linked list
   public void enqueue(T newE){
-    Node<T> newEntry = new Node<>(newE);
-    
-    tail.next.next = newEntry; //add to end
-    tail.next = newEntry;
-    if(isEmpty()){  //first entry to be added, must point head to first Node
-      head.next = tail.next;
-    }
+	  Node<T> newEntry = new Node<>(newE);
+	  if(tail != null)
+		  tail.next = newEntry; //add to end
+	  tail = newEntry;
+	  if(isEmpty()) {
+		  head = newEntry;
+	  }
   }
   
   public T dequeue(){
-    if(!isEmpty()){   //if possible to remove a Node
-      head.next = head.next.next;     //This SHOULD take care of a 1-Node queue by pointing
+	  if(!isEmpty()){   //if possible to remove a Node
+    	T toReturn = head.val;
+    	head = head.next;     //This SHOULD take care of a 1-Node queue by pointing
                                       //head.next = null. CHECK************* b/c might cause null.
-    } 
-    return null;
+    	return toReturn;
+	  } 
+	  else
+		  return null;
   }
   
   public boolean isEmpty(){
-    return ( head.next == null);
+	  return ( head == null);
   }
   
   public void clear(){
-    if(!isEmpty()) {
-    	head.next = null;
-    	tail.next = null;
-    }
+	  if(!isEmpty()) {
+		  head.next = null;
+		  tail.next = null;
+	  }
   }
   
   
